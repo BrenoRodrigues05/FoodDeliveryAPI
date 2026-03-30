df6d57598315a4b6ff28f92cc39d3868d56bb240
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-03-30 20:31:59 -0300

    adiciona Unit of work e Repo de pedidos ao program

--- FoodDeliveryAPI/Program.cs
+++ FoodDeliveryAPI/Program.cs
@@ -1,5 +1,7 @@
 using FoodDeliveryAPI.Application.Middlewares;
 using FoodDeliveryAPI.Context;
+using FoodDeliveryAPI.Infrastructure.Repositories;
+using FoodDeliveryAPI.Infrastructure.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,11 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<FoodDeliveryAPIContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+// Register repositories and unit of work for dependency injection
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 
 var app = builder.Build();
 
