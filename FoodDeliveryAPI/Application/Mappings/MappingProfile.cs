e9c3076915db85955ab2e2520d18c2fd5bb0c4ce
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-04-13 17:25:20 -0300

    adiciona automapper

---
+++ FoodDeliveryAPI/Application/Mappings/MappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FoodDeliveryAPI.Application.DTOs;
+using FoodDeliveryAPI.Domains.Entities;
+
+namespace FoodDeliveryAPI.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapping configurations for Cliente
+            CreateMap<Cliente, ClienteCreateDTO>().ReverseMap();
+            CreateMap<Cliente, ClienteResponseDTO>();
+
+            // Mapping configurations for Pedido
+            CreateMap<Pedido, PedidoCreateDTO>().ReverseMap();
+            CreateMap<Pedido, PedidoResponseDTO>().ReverseMap();
+
+            // Mapping configurations for PedidoItem
+            CreateMap<PedidoItem, PedidoItemResponseDTO>();
+            CreateMap<PedidoItemCreateDTO, PedidoItem>();
+
+            // Mapping configurations for Produto
+            CreateMap<Produto, ProdutoCreateDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoResponseDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoUpdateDTO>();
+
+            // Mapping configurations for Entregador
+            CreateMap<Entregador, EntregadorCreateDTO>().ReverseMap();
+            CreateMap<Entregador, EntregadorResponseDTO>();
+
+            // Mapping configurations for Endereço
+            CreateMap<Endereco, EnderecoCreateDTO>().ReverseMap();
+            CreateMap<Endereco, EnderecoResponseDTO>();
+
+            // Mapping configurations for Produto
+            CreateMap<Produto, ProdutoCreateDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoResponseDTO>();
+            CreateMap<Produto, ProdutoUpdateDTO>().ReverseMap();
+
+        }
+    }
+}
