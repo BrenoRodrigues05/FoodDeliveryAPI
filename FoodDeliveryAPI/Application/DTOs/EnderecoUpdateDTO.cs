8fbfd2c71f4cff41e0b6419bb08e0a94567e6867
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-04-15 20:44:51 -0300

    adiciona update para DTOs de Endereço

---
+++ FoodDeliveryAPI/Application/DTOs/EnderecoUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDeliveryAPI.Application.DTOs
+{
+    public class EnderecoUpdateDTO
+    {
+        public string Nome { get; set; } 
+        public string Rua { get; set; } 
+
+        public string Numero { get; set; } 
+    }
+}
