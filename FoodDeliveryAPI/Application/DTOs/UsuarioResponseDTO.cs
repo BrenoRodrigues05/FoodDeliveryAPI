1ec68d2c7f41785b6e2bf3424b2f2abba83c4ec4
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-04-29 19:26:52 -0300

    cria DTO para resposta em autenticação

---
+++ FoodDeliveryAPI/Application/DTOs/UsuarioResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDeliveryAPI.Application.DTOs
+{
+    public class UsuarioResponseDTO
+    {
+        public string Email { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
